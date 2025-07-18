f2a6e4bee465921388ca0e9191ff120344673ead
Demonkinng <angelchuncho5@gmail.com>
2025-07-18 12:35:30 -0500

    Add menu options and integrate Firestore for player ranking

--- app/src/main/java/com/chuncho/angel/cazarpatos/MainActivity.kt
+++ app/src/main/java/com/chuncho/angel/cazarpatos/MainActivity.kt
@@ -1,17 +1,24 @@
 package com.chuncho.angel.cazarpatos
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import android.media.AudioAttributes
 import android.media.SoundPool
+import android.net.Uri
 import android.os.CountDownTimer
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 import kotlin.random.Random
 
 
@@ -49,6 +56,7 @@ class MainActivity : AppCompatActivity() {
         //Obtener el usuario de pantalla login
         val extras = intent.extras ?: return
         var usuario = extras.getString(EXTRA_LOGIN) ?: "Unknown"
+        usuario = usuario.substringBefore("@") // Extraer solo el nombre de usuario antes del '@'
         textViewUser.setText(usuario)
         //Determina el ancho y largo de pantalla
         initializeScreen()
@@ -102,7 +110,7 @@ class MainActivity : AppCompatActivity() {
         val maximoY = screenHeight - imageViewDuck.getHeight()
         // Generamos 2 números aleatorios, para la coordenadas x , y
         val randomX = Random.nextInt(0, maximoX - min + 1)
-        val randomY = Random.nextInt(0, maximoY - min + 1)
+        val randomY = Random.nextInt(92, maximoY - min + 1)
 
         imageViewDuck.animate()
             .x(randomX.toFloat())
@@ -121,6 +129,9 @@ class MainActivity : AppCompatActivity() {
             textViewTime.setText("0s")
             gameOver = true
             showGameOverDialog()
+            val nombreJugador = textViewUser.text.toString()
+            val patosCazados = textViewCounter.text.toString()
+            procesarPuntajePatosCazados(nombreJugador, patosCazados.toInt()) // Firestore
         }
     }
 
@@ -133,11 +144,15 @@ class MainActivity : AppCompatActivity() {
         builder
             .setMessage(getString(R.string.dialog_message_congratulations, counter))
             .setTitle(getString(R.string.dialog_title_game_end))
+            .setIcon(R.drawable.duck)
             .setPositiveButton(getString(R.string.button_restart)) { _, _ ->
                 restartGame()
             }
             .setNegativeButton(getString(R.string.button_close)) { _, _ ->
                 // Dialog dismisses automatically
+                val intent = Intent(this, RankingActivity::class.java)
+                startActivity(intent)
+                finish()
             }
             .setCancelable(false)  // Prevents closing on outside click
         builder.create().show()
@@ -150,6 +165,109 @@ class MainActivity : AppCompatActivity() {
         textViewCounter.setText(counter.toString())
         moveDuckRandomly()
         initializeCountdown()
+    }
+
+    fun jugarOnline() {
+        var intentWeb = Intent()
+        intentWeb.action = Intent.ACTION_VIEW
+        intentWeb.data = Uri.parse("https://duckhuntjs.com/")
+        startActivity(intentWeb)
+    }
+
+    fun procesarPuntajePatosCazados(nombreJugador: String, patosCazados: Int) {
+        val jugador = Player(nombreJugador, patosCazados)
+        //Trata de obtener id del documento del ranking específico,
+        // si lo obtiene lo actualiza, caso contrario lo crea
+        val db = Firebase.firestore
+        db.collection("ranking")
+            .whereEqualTo("username", jugador.username)
+            .get()
+            .addOnSuccessListener { documents ->
+                if (documents != null &&
+                    documents.documents != null &&
+                    documents.documents.count() > 0
+                ) {
+                    val idDocumento = documents.documents.get(0).id
+                    val jugadorLeido = documents.documents.get(0).toObject(Player::class.java)
+                    if (jugadorLeido!!.huntedDucks < patosCazados) {
+                        Log.w(EXTRA_LOGIN, "Puntaje actual mayor, por lo tanto actualizado")
+                        actualizarPuntajeJugador(idDocumento, jugador)
+                    } else {
+                        Log.w(EXTRA_LOGIN, "No se actualizo puntaje, por ser menor al actual")
+                    }
+                } else {
+                    ingresarPuntajeJugador(jugador)
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(EXTRA_LOGIN, "Error getting documents", exception)
+                Toast.makeText(this, "Error al obtener datos de jugador", Toast.LENGTH_LONG).show()
+            }
+    }
+
+    fun ingresarPuntajeJugador(jugador: Player) {
+        val db = Firebase.firestore
+        db.collection("ranking")
+            .add(jugador)
+            .addOnSuccessListener { documentReference ->
+                Toast.makeText(this, "Puntaje usuario ingresado exitosamente", Toast.LENGTH_LONG)
+                    .show()
+            }
+            .addOnFailureListener { exception ->
+                Log.w(EXTRA_LOGIN, "Error adding document", exception)
+                Toast.makeText(this, "Error al ingresar el puntaje", Toast.LENGTH_LONG).show()
+            }
+    }
+
+    fun actualizarPuntajeJugador(idDocumento: String, jugador: Player) {
+        val db = Firebase.firestore
+        db.collection("ranking")
+            .document(idDocumento)
+            //.update(contactoHashMap)
+            .set(jugador) //otra forma de actualizar
+            .addOnSuccessListener {
+                Toast.makeText(
+                    this,
+                    "Puntaje de usuario actualizado exitosamente",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+            .addOnFailureListener { exception ->
+                Log.w(EXTRA_LOGIN, "Error updating document", exception)
+                Toast.makeText(this, "Error al actualizar el puntaje", Toast.LENGTH_LONG).show()
+            }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_nuevo_juego -> {
+                restartGame()
+                true
+            }
+
+            R.id.action_jugar_online -> {
+                jugarOnline()
+                true
+            }
+
+            R.id.action_ranking -> {
+                val intent = Intent(this, RankingActivity::class.java)
+                startActivity(intent)
+                true
+            }
+
+            R.id.action_salir -> {
+                finish()
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     override fun onStop() {
--- app/src/main/java/com/chuncho/angel/cazarpatos/RankingActivity.kt
+++ app/src/main/java/com/chuncho/angel/cazarpatos/RankingActivity.kt
@@ -2,12 +2,17 @@ package com.chuncho.angel.cazarpatos
 
 import RankingPlayerDBHelper
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.firestore
 
 class RankingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,6 +24,7 @@ class RankingActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }*/
+        consultarPuntajeJugadores()
 //        var jugadores = arrayListOf<Player>()
 //        jugadores.add(Player("Angel.Chuncho", 10))
 //        jugadores.add(Player("Jugador2", 6))
@@ -33,6 +39,32 @@ class RankingActivity : AppCompatActivity() {
         OperacionesSqLite()
         GrabarRankingSQLite()
         LeerRankingsSQLite()
+    }
+
+    private fun consultarPuntajeJugadores() {
+        val db = Firebase.firestore
+        db.collection("ranking")
+            .orderBy("huntedDucks", Query.Direction.DESCENDING)
+            .get()
+            .addOnSuccessListener { result ->
+                Log.d(EXTRA_LOGIN, "Success getting documents")
+                var jugadores = ArrayList<Player>()
+                for (document in result) {
+                    val jugador = document.toObject(Player::class.java)
+                    //val jugador = document.toObject<Player>()
+                    jugadores.add(jugador)
+                }
+                //Poblar en RecyclerView información usando mi adaptador
+                val recyclerViewRanking: RecyclerView = findViewById(R.id.recyclerViewRanking);
+                recyclerViewRanking.layoutManager = LinearLayoutManager(this);
+                recyclerViewRanking.adapter = RankingAdapter(jugadores);
+                recyclerViewRanking.setHasFixedSize(true);
+            }
+            .addOnFailureListener { exception ->
+                Log.w(EXTRA_LOGIN, "Error getting documents.", exception)
+                Toast.makeText(this, "Error al obtener datos de jugadores", Toast.LENGTH_LONG)
+                    .show()
+            }
     }
 
     fun OperacionesSqLite() {
